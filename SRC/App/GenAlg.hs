9966bf24d782033053238aebca1a5c0094e0e32b
Evgene Oskin <burnst@mail.ru>
2012-08-09 14:00:22 +0700

    working genetic algorithm

--- SRC/App/GenAlg.hs
+++ SRC/App/GenAlg.hs
@@ -8,7 +8,7 @@ module SRC.App.GenAlg ( findStablePortfolio
 import SRC.App.PoolGenerator
 import SRC.App.UsefullFunc
 import Data.Maybe
-import Data.List (sum, map)
+import Data.List (sum, map, nub)
 import System.Random (mkStdGen, random, randoms)
 import System.IO(IOMode(..))
 import GA (Entity(..), GAConfig(..), 
@@ -17,56 +17,69 @@ import GA (Entity(..), GAConfig(..),
 instance Entity [(String, [Double])] Double Int [(String, [Double])] IO where
 
     -- generate a random entity, i.e. a random list of quot  
-    genRandom pool seed = return $ take n $ Prelude.map ((!!) pool) is
+    genRandom pool seed = return $ take n $ Data.List.map ((!!) pool) is
         where
           g = mkStdGen seed
-          n = (fst $ random g) `mod` 4
+          --n = 1 + (fst $ random g) `mod` 3
+          n = 3
           k = length pool
-          is = Prelude.map (flip mod k) $ randoms g
+          notIs = Data.List.map (flip mod k) $ randoms g
+          is = Data.List.nub notIs    
  
     -- crossover operator: mix (and trim to shortest entity)
-    crossover _ _ seed e1 e2 = return $ Just e
+    crossover _ _ seed e1 e2 = return $ Just $ take 3 $ Data.List.nub $ e ++ e1 ++ e2
         where
           g = mkStdGen seed
-          cps = zipWith (\x y -> [x, y]) e1 e2
-          picks = Prelude.map (flip mod 2) $ randoms g
+          cps = zipWith (\ x y -> [x, y]) e1 e2
+          picks = Data.List.map (flip mod 2) $ randoms g
           e = zipWith (!!) cps picks
 
     -- mutation operator: delete old and add new random quotes (max. 2)
-    mutation pool p seed e = return $ Just $ (drop x e) ++ addQuotes
+    mutation pool p seed e = return $ Just $ take 3 $ Data.List.nub $ (drop x e) ++ addQuotes
         where
           g = mkStdGen seed
           k = round (p*4.0) :: Int
           x = mod (fst (random g)) k
-          is = Prelude.map (flip mod $ length pool) $ randoms g
-          addQuotes = take x $ Prelude.map ((!!) pool) is
+          is = Data.List.map (flip mod $ length pool) $ randoms g
+          addQuotes = Data.List.map ((!!) pool) is
 
     -- calculate the goodness of quotes
-    score' _ eAscList = Just goodness 
-        where
-          e = map (\(a,b) -> b) eAscList
-        -- e is List of Lists
-          n = fromIntegral $ length $ e !! 0
-          buyPrice = funOfSomeElement (+) e 0
-          sumList = funOfListsElements (+) e
-          goodness = (Data.List.sum $Data.List.map (\ x -> (x - buyPrice)**2.0) sumList) / n
+    score _ eAscList = do
+        let
+            e = map (\(a,b) -> b) eAscList              
+            n = fromIntegral $ length $ e !! 0
+            buyPrice = funOfSomeElement (+) e 0
+            sumList = funOfListsElements (+) e
+            goodness = (Data.List.sum $Data.List.map (\ x -> (x - buyPrice)**2.0) sumList) / n
+        --print e
+        return $ Just goodness 
 
     -- whether or not a scored entity is perfect
     isPerfect (_, s) = s == 0.0
 
-findStablePortfolio :: String -> String -> (String, String) -> IO ()
-findStablePortfolio market inDN timePeriod =
+findStablePortfolio :: String
+                    -> String
+                    -> [String]
+                    -> ( Int
+                       , Int
+                       , Int
+                       , Float
+                       , Float
+                       , Float
+                       , Float)
+                    -> IO ()
+findStablePortfolio market inDN timePeriod (population, bests, maxGen, crossRate, mutRate, crossPar, mutPar) =
     do
       let cfg = GAConfig 
-                100 -- population size
-                25 -- archive size (best entities to keep track of)
-                300 -- maximum number of generations
-                0.8 -- crossover rate (% of entities by crossover)
-                0.5 -- mutation rate (% of entities by mutation)
-                0.0 -- parameter for crossover (not used here)
-                0.75 -- parameter for mutation (% of replaced letters)
-                False -- whether or not to use checkpointing
-                False -- don't rescore archive in each generation
+                population -- 1000 -- population size
+                bests      -- 50   -- archive size (best entities to keep track of)
+                maxGen     -- 300  -- maximum number of generations
+                crossRate  -- 0.8  -- crossover rate (% of entities by crossover)
+                mutRate    -- 0.5  -- mutation rate (% of entities by mutation)
+                crossPar   -- 0.0  -- parameter for crossover (not used here)
+                mutPar     -- 0.2  -- parameter for mutation (% of replaced letters)
+                False              -- whether or not to use checkpointing
+                False              -- don't rescore archive in each generation
 
           g = mkStdGen 0 -- random generator
                        
@@ -74,6 +87,6 @@ findStablePortfolio market inDN timePeriod =
       quotesPool <- makeTotalPool market inDN timePeriod
       -- Do the evolution!
       es <- evolveVerbose g cfg quotesPool 1
-      let e = snd $ head es
+      let e = snd $ head es :: [(String, [Double])]
           
-      putStrLn $ "best entity (GA): " ++ (show $ map (\(a,b) -> a) e)
+      putStrLn $ "best entity (GA): " ++ (show $ Data.List.map (\(a,b) -> a) e)
